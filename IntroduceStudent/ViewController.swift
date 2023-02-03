856f656e2faccf95b5f4b0792817942ed7d42a64
Abdoulaye Sedego <abdoulaye.sedego95@gmail.com>
2023-02-03 08:07:00 -0500

    update code logic to fix major bug

--- IntroduceStudent/ViewController.swift
+++ IntroduceStudent/ViewController.swift
@@ -17,8 +17,27 @@ class ViewController: UIViewController {
     @IBOutlet weak var schoolNameTextField: UITextField!
     
     @IBOutlet weak var yearSegmentedControl: UISegmentedControl!
-    
     @IBOutlet weak var numberOfPetsLabel: UILabel!
+    
+    @IBAction func stepperDidChange(_ sender: UIStepper) {
+        numberOfPetsLabel.text = "\(Int(sender.value))"
+    }
+    
+    @IBAction func introduceSelfDidTapped(_ sender: UIButton) {
+        let year = yearSegmentedControl.titleForSegment(at: yearSegmentedControl.selectedSegmentIndex)
+        
+        let introduction = "My name is \(firstNameTextField.text!) \(lastNameTextField.text!) and I attend \(schoolNameTextField.text!). I am currently in my \(year!) year and I own \(numberOfPetsLabel.text!) dogs. It is \(morePetsSwitch.isOn) that I want more pets."
+        
+        print(introduction)
+        
+        let alertController = UIAlertController(title: "My Introduction", message: introduction, preferredStyle: .alert)
+        
+        let action = UIAlertAction(title: "Nice to meet you!", style: .default, handler: nil)
+        
+        alertController.addAction(action)
+                
+        present(alertController, animated: true, completion: nil)
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
